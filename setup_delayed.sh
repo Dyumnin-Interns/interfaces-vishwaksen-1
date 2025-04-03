1e49d467b1ac25f3ed41ac687b4efed75ceecf2b
Vijayvithal Jahagirdar <jvs@dyumnin.com>
2025-04-03 17:12:47 +0530

    fixing mandb timeout issue.

--- setup_delayed.sh
+++ setup_delayed.sh
@@ -1,3 +1,5 @@
+echo "set man-db/auto-update false" |sudo debconf-communicate
+sudo dpkg-reconfigure man-db
 sudo apt install -y --no-install-recommends iverilog
 pip3 install cocotb cocotb-bus cocotb-coverage
 cp hdl/delayed_dut.v hdl/dut.v
